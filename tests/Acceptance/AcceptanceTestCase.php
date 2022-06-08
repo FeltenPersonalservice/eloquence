a40d7b6f3e866f7cb23262fb3acec3dde1c9c6fa
Sascha Gehlich <hello@sash.wtf>
2022-06-08 15:11:55 +0200

    Add Laravel 9 support

--- tests/Acceptance/AcceptanceTestCase.php
+++ tests/Acceptance/AcceptanceTestCase.php
@@ -7,7 +7,7 @@ use Orchestra\Testbench\TestCase;
 
 class AcceptanceTestCase extends TestCase
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
